a0387b7d55a3ce03a2898d72a574b118716db767
Kapil Bansal <110424577+kapi36@users.noreply.github.com>
2024-01-20 11:26:23 +0530

    ram.v

--- ram.v
+++ ram.v
@@ -1,21 +1,22 @@
-module ram(input clk, input wr , input rst, 
-           input [7:0] din, input [7:0] addr,
-           output reg [7:0] dout);   
-  reg [7:0]mem[64]; 
-  integer i ;
-  
-  always @(posedge clk) begin 
-    if(rst==1'b1) begin 
-      for(i= 0 ; i<256; i++)begin 
-        mem[i] <= 0 ; 
-      end 
-    end
-    else if(wr == 1) 
-      mem[addr] = din ; 
-    else 
-      dout <= mem[addr] ; 
-  end
-endmodule 
-       
-      
-      
+module ram(
+input clk, wr,rst ,
+input [7:0] din, addr,
+output reg [7:0] dout
+);
+reg [7:0] mem [256];
+integer i;
+ 
+always@(posedge clk)
+begin
+if(rst == 1'b1) begin
+for( i = 0; i < 256; i++)
+begin
+mem[i] <= 0;
+end
+end
+else if(wr == 1'b1)
+mem[addr] <= din;
+else
+dout <= mem[addr];
+end
+endmodule
