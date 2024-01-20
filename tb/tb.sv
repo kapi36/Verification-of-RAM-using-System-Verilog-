07c7222f4f8b58daaf043c6a6d43d4d86468d3fa
Kapil Bansal <110424577+kapi36@users.noreply.github.com>
2024-01-20 11:25:26 +0530

    tb.sv

--- tb/tb.sv
+++ tb/tb.sv
@@ -1,176 +1,202 @@
-class transaction ; 
-  randc bit[7:0] din ; 
-  randc bit[7:0] addr; 
-  bit[7:0] dout; 
-  bit wr ; 
-endclass 
-
- class generator ; 
-   mailbox mbx; 
-   transaction t;
-   event done; 
-   integer i ; 
-   
-   function new(mailbox mbx); 
-     this.mbx = mbx; 
-   endfunction 
-   
-   task run(); 
-     t  = new(); 
-     for(i=0 ; i<50; i++) begin 
-       t.randomize(); 
-       mbx.put(t); 
-       $display("[GEN]:Data send to driver din : %0d , addr;%0d", t.din , t.addr); 
-       @(done); // sensing the event 
-     end
-   endtask 
- endclass
-
-
-
-interface ram_intf(); 
-
-logic clk, wr ,rst; 
-logic [7:0] din ; 
-logic [7:0] addr; 
-logic [7:0] dout; 
+interface counter_intf();
+logic clk,rst, wr;
+logic [7:0] din, addr;
+logic [7:0] dout;
 endinterface
-
-
- class driver ; 
-   mailbox mbx; 
-   transaction t;
-   event done; 
-   virtual ram_intf vif; 
-   
-   function new(mailbox mbx); 
-     this.mbx = mbx; 
-   endfunction 
-   
-   task run(); 
-     t  = new(); 
-     forever begin 
-       mbx.get(t); 
-       vif.din = t.din ; 
-       vif.addr = t.addr; 
-       $display("[DRV]:Interface Triggered din : %0d , addr: %0d wr:%0d", t.din , t.addr,vif.wr); 
-       @(posedge vif.clk) ; 
-       
-       if(t.wr == 1'b0)
-         @(posedge vif.clk)
-      
-       ->(done); // triggering the event 
-     end
-   endtask 
- endclass 
-
-class monitor ; 
-  mailbox mbx; 
-  transaction t; 
-  virtual ram_intf vif; 
-   
-   function new(mailbox mbx); 
-     this.mbx = mbx; 
-   endfunction 
+ 
+/////////////////////Transaction
+class transaction;
+rand bit [7:0] din;
+randc bit [7:0] addr;
+bit wr;
+bit [7:0] dout;
   
-  task run(); 
-     t  = new(); 
-     forever begin 
-       @(posedge vif.clk) ;
-       
-       if(t.wr == 1'b1)begin
-       t.wr = vif.wr ; 
-       t.din = vif.din  ; 
-       t.addr = vif.addr;
-       t.dout = 0;
-       end
-       
-       if(t.wr == 1'b0) begin 
-         @(posedge vif.clk) ;
-       t.wr = vif.wr ; 
-       t.din = vif.din  ; 
-       t.addr = vif.addr;
-       t.dout = vif.dout;
-       end
-       
-       mbx.put(t);  
-       $display("[MON]:Data send to Scoreboard : %0d , addr: %0d wr:%0d", t.din , t.addr,vif.wr);  
-     end
-  endtask 
-endclass 
-
-class scoreboard ;
-  transaction t; 
-  mailbox mbx; 
-  reg [7:0]tarr[256] = '{default : 0 } ; 
+constraint addr_c {addr > 10; addr < 18;};  
+endclass
+ 
+ 
+/////////////////////Generator
+class generator;
+mailbox mbx;
+transaction t;
+event done;
+integer i;
+ 
+function new(mailbox mbx);
+this.mbx = mbx;
+endfunction
+ 
+task run();
+t = new();
+for(i = 0; i < 100; i++)begin
+t.randomize();
+mbx.put(t);
+  $display("[GEN] : Data send to driver din : %0d and addr : %0d",t.din, t.addr);
+@(done);
+end
+endtask
+endclass
+ 
+////////////////////Driver
+ 
+class driver;
+mailbox mbx;
+event done;
+transaction t;
+virtual counter_intf vif;
+ 
+function new(mailbox mbx);
+this.mbx = mbx;
+endfunction
+ 
+task run();
+t = new();
+forever begin
+mbx.get(t);
+vif.din = t.din;
+vif.addr = t.addr;
+$display("[DRV] : Interface Trigger wr : %0b addr : %0d din : %0d",vif.wr, t.addr, t.din);
+@(posedge vif.clk);
+ 
   
-   function new(mailbox mbx); 
-     this.mbx = mbx; 
-   endfunction 
+if(vif.wr == 1'b0) begin
+    @(posedge vif.clk);
+end
   
-  task run() ; 
-    forever begin 
-      mbx.get(t) ; 
-      if(t.wr ==1'b1)begin
-        tarr[t.addr] = t.din ; 
-        $display("[SCO] : Data stored din : %0d addr : %0d ", t.din,t.addr);
-      end
-      
-      if (t.wr ==1'b0)begin
-        if(t.dout == 0 ) 
-          $display("[SCO] : No Data Written at this Location Test Passed");
-        else if (t.dout == tarr[t.addr])
-           $display("[SCO] : Valid Data found Test Passed");
-        else
-          $display("Test failed "); 
-      end
-    end
-  endtask 
-endclass 
-
-class enviornment ; 
-  generator g ; 
-  driver d ; 
-  monitor m; 
-  scoreboard s; 
-  mailbox gdmbx; 
-  mailbox msmbx;
-event gddone ; 
+->done;
+ 
   
-  virtual ram_intf vif ; 
   
-  function new(mailbox gdmbx , mailbox msmbx) ; 
-    this.gdmbx = gdmbx;
-    this.msmbx = msmbx ; 
-    g = new(gdmbx) ; 
-    d = new(gdmbx ) ; 
-    m = new(msmbx); 
-    s = new(msmbx); 
-  endfunction
+end
+endtask
+endclass  
+ 
+ 
+////////////////////Monitor
+class monitor;
+mailbox mbx;
+virtual counter_intf vif;
+transaction t;
+ 
+function new(mailbox mbx);
+this.mbx = mbx;
+endfunction
+ 
+task run();
+t = new();
+forever begin
+@(posedge vif.clk);
+ 
+if(t.wr == 1'b1) begin  
+t.din = vif.din;
+t.addr = vif.addr;
+t.dout = 0;
+t.wr = vif.wr;
+end
+ 
+if(t.wr == 1'b0) begin 
+@(posedge vif.clk); 
+t.din = vif.din;
+t.addr = vif.addr;
+t.dout = vif.dout;
+t.wr = vif.wr; 
+end  
   
-  task run (); 
-    g.done = gddone ; 
-    d.done = gddone ; 
-     d.vif = vif; 
-    m.vif = vif; 
     
-    fork 
-      g.run(); 
-      d.run(); 
-      m.run(); 
-      s.run(); 
-    join 
-  endtask 
-endclass 
+mbx.put(t);
+$display("[MON] : data send to SCO wr :%0b din : %0d addr : %0d dout : %0d ", t.wr,t.din,t.addr,t.dout); 
+end
+endtask
+endclass
  
-
-module tb();
  
- enviornment e;
-ram_intf vif();
+////////////////Scoreboard
+class scoreboard;
+mailbox mbx;
+//transaction tarr[256];
+  
+reg [7:0] tarr[256] = '{default:0}; 
+transaction t;
+ 
+function new(mailbox mbx);
+this.mbx = mbx;
+endfunction
+ 
+task run();
+forever begin
+mbx.get(t);
+ 
+ 
+  if(t.wr == 1'b1) begin
+    tarr[t.addr] = t.din;
+    $display("[SCO] : Data stored din : %0d addr : %0d ", t.din,t.addr);
+   end
+  
+  if(t.wr == 1'b0) begin
+    if(t.dout == 0)
+      $display("[SCO] : No Data Written at this Location Test Passed");
+    else if (t.dout == tarr[t.addr])
+      $display("[SCO] : Valid Data found Test Passed");
+    else
+      $display("[SCO] : Test Failed");   
+  end   
+end
+endtask
+endclass
+ 
+ 
+//////////////////////Env
+class environment;
+generator gen;
+driver drv;
+monitor mon;
+scoreboard sco;
+ 
+virtual counter_intf vif;
+ 
+mailbox gdmbx;
+mailbox msmbx;
+ 
+event gddone;
+ 
+function new(mailbox gdmbx, mailbox msmbx);
+this.gdmbx = gdmbx;
+this.msmbx = msmbx;
+ 
+gen = new(gdmbx);
+drv = new(gdmbx);
+ 
+mon = new(msmbx);
+sco = new(msmbx);
+endfunction
+ 
+task run();
+gen.done = gddone;
+drv.done = gddone;
+ 
+drv.vif = vif;
+mon.vif = vif;
+ 
+fork 
+gen.run();
+drv.run();
+mon.run();
+sco.run();
+join_any
+ 
+endtask
+ 
+endclass
+ 
+ 
+////////////////////Testbench Top
+module tb;
+ 
+environment env;
+counter_intf vif();
 mailbox gdmbx, msmbx;
  
-ram dut (vif.clk, vif.wr,vif.rst, vif.din, vif.addr, vif.dout);
+ram dut (vif.clk, vif.rst, vif.wr, vif.din, vif.addr, vif.dout);
  
 always #5 vif.clk = ~vif.clk;
  
@@ -193,10 +219,10 @@ initial begin
 gdmbx = new();
 msmbx = new();
  
-e = new(gdmbx,msmbx);
-e.vif = vif;
+env = new(gdmbx,msmbx);
+env.vif = vif;
 #50;  
-e.run();
+env.run();
 #600;
 $finish;
 end
@@ -209,5 +235,3 @@ end
  
  
 endmodule
-      
-  
